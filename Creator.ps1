a558c45ffe248dc658bd9971b6b2327eb62ee06a
Nour Saif Yassine <nour.saif.yassine@gmail.com>
2025-04-07 14:09:52 +0200

    add: creator main features

---
+++ Creator.ps1
@@ -0,0 +1,75 @@
+$dir = "$HOME/Github_Projects/";
+
+function Set-GithubDir {
+    if (-not (Test-Path -Path $dir)) {
+        New-Item -Path $dir -ItemType Directory -Force | Out-Null;
+        [string]$inputProjectName = Read-Host "Enter the name of your project";
+        $dir = "$dir$inputProjectName/";
+        New-Item -Path $dir -ItemType Directory -Force | Out-Null;
+        Write-Host "Directory created at $dir";
+        Set-Location -Path $dir;
+        Set-NodeJSDir;
+    }
+    else {
+        New-Item -Path $dir -ItemType Directory -Force | Out-Null;
+        [string]$inputProjectName = Read-Host "Enter the name of your project";
+        $dir = "$dir$inputProjectName/";
+        New-Item -Path $dir -ItemType Directory -Force | Out-Null;
+        Set-Location $dir
+        Set-NodeJSDir
+    }
+
+    return $dir;
+}
+
+function Set-NodeJSDir {
+    Set-Location $dir;
+    
+    [string]$inputNodeJSChoice = Read-Host "Do you want to add NodeJS to your project? (y/n)";
+    if ($inputNodeJSChoice -eq "y") {
+        npm init -y
+        Write-Host "Writing to package.json...";
+        Set-ExpressAndSQLite;
+        Set-BootstrapOrTailwindCSS;
+        Write-Host "package.json created successfully!";
+    }
+    else {
+        Write-Host "Continuing without creating a NodeJS project...";
+        return;
+    }
+}
+
+function Set-BootstrapOrTailwindCSS {
+    [string]$inputCSSChoice = Read-Host "Do you want to add Bootstrap or Tailwind CSS to your project? (b/t/n)";
+
+    if ($inputCSSChoice -eq "b") {
+        Write-Host "Installing Bootstrap...";
+        node.exe npm install bootstrap --save;
+        Write-Host "Bootstrap installed successfully!";
+    }
+    elseif ($inputCSSChoice -eq "t") {
+        Write-Host "Installing Tailwind CSS...";
+        npm install tailwindcss @tailwindcss/cli --save-dev;
+        New-Item -ItemType Directory -Path "src" -Force | Out-Null;
+        New-Item -ItemType File -Path "src/input.css" -Force | Out-Null;
+        Add-Content -Path "src/input.css" -Value "@'tailwindcss';";
+        Write-Host "Tailwind CSS installed successfully!";
+    }
+    elseif ($inputCSSChoice -eq "n") {
+        Write-Host "Continuing without adding CSS framework...";
+    }
+}
+
+function Set-ExpressAndSQLite {
+    [string]$inputExpressChoice = Read-Host "Do you want to add Express and SQLite to your project? (y/n)";
+    if ($inputExpressChoice -eq "y") {
+        Write-Host "Installing Express and SQLite...";
+        npm install express sqlite3 --save;
+        Write-Host "Express and SQLite installed successfully!";
+    }
+    else {
+        Write-Host "Continuing without adding Express and SQLite...";
+    }
+}
+
+Set-GithubDir;
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Project_Creator_PS1
 An installation script that installs certain libraries and goes into the chosen folder to be working in.
+
+## Features
+    - [x]  Choose Tailwind or Bootstrap
+    - [x]  Choose NodeJS or not
+    - [x]  Create project folder inside Github_Projects
+    - [x]  Create Github_Projects folder if needed
+    - [x]  Navigate to the Github_Projects
+    - [x]  If NodeJS chosen express and sqlite will be installed by choice.
+    - [ ]  Edit the README.md to show how to configure enviroment variables
